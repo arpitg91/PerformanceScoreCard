f230698fcaa8c2aade0daafd5b6adf5a4ece22f6
Arpit Goel <arpit.g91@gmail.com>
2017-03-14 21:55:31 +0530

    Final Check in
    Check in of the script,

--- performance.py
+++ performance.py
@@ -5,137 +5,106 @@ from sklearn.metrics import precision_recall_curve, average_precision_score
 from matplotlib import use
 use("Agg")
 import matplotlib.pyplot as plt
-
-def get_roc(df,score,target):
-    fpr, tpr, thresholds = roc_curve(df[target], df[score])
-    results = pd.DataFrame(np.reshape(np.hstack([tpr,fpr,thresholds]),(fpr.shape[0],3)),columns = ["tpr","fpr","threshholds"])
-    results.to_csv("auc_curve.csv",index=False)
-    print results.describe()
-    roc_auc = auc(fpr, tpr)
-    print "Area under the ROC curve : %f" % roc_auc
+import csv,argparse
+import warnings
+warnings.filterwarnings("ignore")
+# For plotting inline in notebook
+# %matplotlib inline 
+def get_roc(df,score,target,title,plot=1):
+    df1 = df[[score,target]].dropna()
+    fpr, tpr, thresholds = roc_curve(df1[target], df1[score])
+    ks=np.abs(tpr-fpr)
+    if plot==1:
     # Plot ROC curve
-    plt.figure()
-    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.figure(figsize=(6,4))
+        plt.plot(fpr, tpr, label='AUC=%0.2f KS=%0.2f' %(auc(fpr, tpr),ks.max()))
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.grid(b=True, which='both', color='0.65',linestyle='-')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title(title+'Receiver Operating Characteristic')
+        plt.legend(loc="lower right")
+    return auc(fpr, tpr),np.max(np.abs(tpr-fpr)),thresholds[ks.argmax()]
+def get_cum_gains(df,score,target,title):
+    df1 = df[[score,target]].dropna()
+    fpr, tpr, thresholds = roc_curve(df1[target], df1[score])
+    ppr=(tpr*df[target].sum()+fpr*(df[target].count()-df[target].sum()))/df[target].count()
+    plt.figure(figsize=(12,4))
+    plt.subplot(1,2,1)
+    plt.plot(ppr, tpr, label='')
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic')
+    plt.grid(b=True, which='both', color='0.65',linestyle='-')
+    plt.xlabel('%Population')
+    plt.ylabel('%Target')
+    plt.title(title+'Cumulative Gains Chart')
     plt.legend(loc="lower right")
-    
-    plt.savefig("myfig1.png")
-# Plot ROC curve
 
-def get_precision_recall(df,score,target):
+    plt.subplot(1,2,2)
+    plt.plot(ppr, tpr/ppr, label='')
+    plt.plot([0, 1], [1, 1], 'k--')
+    plt.grid(b=True, which='both', color='0.65',linestyle='-')
+    plt.xlabel('%Population')
+    plt.ylabel('Lift')
+    plt.title(title+'Lift Curve')
+def get_precision_recall(df,score,target,title):
     precision, recall, _ = precision_recall_curve(df[target], df[score])
     roc_pr = average_precision_score(df[target], df[score])
-    print "Area under the Precision-Recall curve : %f" % roc_pr
     # Plot ROC curve
-    plt.clf()
+    plt.figure(figsize=(6,4))
     plt.plot(recall, precision, label='Precision-Recall curve (AUC = %0.2f)' % roc_pr)
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.ylim([0.0, 1.05])
     plt.xlim([0.0, 1.0])
-    plt.title("Precision-Recall Curve")
+    plt.title(title+"Precision-Recall Curve")
     plt.legend(loc="lower left")
-    plt.show()
-    plt.savefig("myfig2.png")
-
-#ROC, AUC, KS, GINI, Bivariate, Precision Recall, Sensitivity and specificity
-
-def get_ks(df,score,target):
-    fpr, tpr, thresholds = roc_curve(df[target], df[score])
-    roc_auc = auc(fpr, tpr)
-    data = df.copy(deep=True)
-    ascending = (data[score].corr(data[target]) < 0)
-    dsd = data.groupby(score)[target].agg(["sum","count" ])
-    dsd.sort_index( ascending = ascending, inplace=True)
-    dsd["ones"] = dsd["sum"]
-    dsd["zeros"] = dsd["count"] - dsd["ones"]
-    dsd["TP"] = dsd["ones"].cumsum()
-    dsd["FP"] = dsd["zeros"].cumsum()
-    dsd["FN"] = dsd["ones"].sum() - dsd["ones"]
-    dsd["TN"] = dsd["zeros"].sum() - dsd["zeros"]
-    dsd["TPR"] = dsd["TP"] /dsd["ones"].sum()
-    dsd["FPR"] = dsd["FP"] /dsd["zeros"].sum()
-    dsd["FNR"] = dsd["FN"] /dsd["ones"].sum()
-    dsd["TNR"] = dsd["TN"] /dsd["zeros"].sum()
-    KS = np.max(dsd["TPR"] - dsd["FPR"])
-    KS_Score = dsd.index.values[np.argmax(dsd["TPR"] - dsd["FPR"])]
-    TPR_KS = dsd["TPR"].values[np.argmax(dsd["TPR"] - dsd["FPR"])]
-    FPR_KS = dsd["FPR"].values[np.argmax(dsd["TPR"] - dsd["FPR"])]
-    print "AUC = %0.3f, KS = %0.3f at score %.3f, TPR = %0.3f, FPR = %0.3f "%(roc_auc, KS, KS_Score, TPR_KS, FPR_KS)
-    plt.savefig("myfig3.png")
-    plt.clf()
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic')
-    plt.ylim([0.0, 1.05])
-    plt.xlim([0.0, 1.0])
-    plt.plot(dsd["FPR"], dsd["TPR"], label='ROC curve (AUC = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.annotate ('', [FPR_KS,FPR_KS], [FPR_KS, TPR_KS], arrowprops={'arrowstyle':'<->'})
-    plt.annotate('KS = %0.2f'%KS, xy=(FPR_KS+ 0.005, (3*TPR_KS + 2*FPR_KS)/5), xycoords = 'data',xytext = (3, 0), textcoords = 'offset points')
-    plt.legend(loc="lower right")
-    plt.figure()
-    plt.show()
-    plt.savefig("myfig4.png")
-
-def get_cum_gains(df,score,target):
-    data = df.copy(deep=True)
-    ascending = (data[score].corr(data[target]) < 0)
-    dsd = data.groupby(score)[target].agg(["sum","count" ])
-    dsd.sort_index( ascending = ascending, inplace=True)
-    dsd["ones"] = dsd["sum"]
-    dsd["zeros"] = dsd["count"] - dsd["ones"]
-    dsd["TP"] = dsd["ones"].cumsum()
-    dsd["FP"] = dsd["zeros"].cumsum()
-    dsd["FN"] = dsd["ones"].sum() - dsd["ones"]
-    dsd["TN"] = dsd["zeros"].sum() - dsd["zeros"]
-    dsd["TOT"] = dsd["count"].cumsum()
-    dsd["TPR"] = dsd["TP"] /np.float(dsd["ones"].sum())
-    dsd["FPR"] = dsd["FP"] /np.float(dsd["zeros"].sum())
-    dsd["FNR"] = dsd["FN"] /np.float(dsd["ones"].sum())
-    dsd["TNR"] = dsd["TN"] /np.float(dsd["zeros"].sum())
-    dsd["TOTR"] = dsd["TOT"] /np.float(dsd["count"].sum())
-    dsd["LIFT"] = dsd["TPR"] /dsd["TOTR"]
-    plt.clf()
-    plt.ylabel('Cumulative Gains')
-    plt.xlabel('Population Rate')
-    plt.title('Cumulative Gains Chart')
-    plt.ylim([0.0, 1.05])
-    plt.xlim([0.0, 1.0])  
-    plt.plot(dsd["FPR"], dsd["TPR"])
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.figure()
-    plt.show()
-    plt.clf()
-    plt.ylabel('Lift')
-    plt.xlabel('Population Rate')
-    plt.title('Lift Curve')
-    plt.ylim([0.99, dsd["LIFT"].max()])
-    plt.xlim([0.0, 1.0])  
-    plt.plot(dsd["FPR"], dsd["LIFT"])
-    plt.plot([0, 1], [1, 1], 'k--')
-    plt.figure()
-    plt.show()
-    plt.savefig("myfig4.png")
-
+    plt.grid(b=True, which='both', color='0.65',linestyle='-')
+def get_deciles_analysis(df,score,target):
+    df1 = df[[score,target]].dropna()
+    _,bins = pd.qcut(df1[score],10,retbins=True)
+    bins[0] -= 0.001
+    bins[-1] += 0.001
+    bins_labels = ['%d.(%0.2f,%0.2f]'%(9-x[0],x[1][0],x[1][1]) for x in enumerate(zip(bins[:-1],bins[1:]))]
+    bins_labels[0] = bins_labels[0].replace('(','[')
+    df1['Decile']=pd.cut(df1[score],bins=bins,labels=bins_labels)
+    df1['Population']=1
+    df1['Zeros']=1-df1[target]
+    df1['Ones']=df1[target]
+    summary=df1.groupby(['Decile'])[['Ones','Zeros','Population']].sum()
+    summary=summary.sort_index(ascending=False)
+    summary['TargetRate']=summary['Ones']/summary['Population']
+    summary['CumulativeTargetRate']=summary['Ones'].cumsum()/summary['Population'].cumsum()
+    summary['TargetsCaptured']=summary['Ones'].cumsum()/summary['Ones'].sum()
+    return summary
 if __name__ == "__main__":
-    mean = np.array([1,1])
-    cov = np.array([[1,0.3],[0.3,1]])
-    score_card = np.random.multivariate_normal(mean,cov,10000)
-    score_card = pd.DataFrame(np.concatenate((score_card,np.reshape(1*(score_card[:,1] > 0.999),(10000,1))),axis=1),columns=["Score","Raw Target","Target"])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ifile", help="Input file")
+    parser.add_argument("--d", help="Delimiter. Default: Comma")
+    parser.add_argument("--score", help="Score Column. Default: score")
+    parser.add_argument("--target", help="Target Column. Default: target")
+    parser.add_argument("--tag", help="Target Column. Default: performance")
+    parser.add_argument("--title", help="Target Column. Default: None")
     
-    score = "Score"
-    target = "Target"
+    args = parser.parse_args()
+    infile = args.ifile
+    score = args.score if args.score else 'score'
+    target = args.target if args.target else 'target'
+    tag = args.tag if args.tag else 'performance'
+    delimiter = args.d if args.d else ','
+    title = args.title+':' if args.title else ''
     
-    ascending = (score_card[score].corr(score_card[target]) < 0)
-    score_card.sort(columns = score, ascending = ascending)
-    #get_roc(score_card,score,target)
-    get_precision_recall(score_card,score,target)
-    get_ks(score_card,score,target)
-    get_cum_gains(score_card,score,target)
+    score_card=pd.read_csv(infile,delimiter=delimiter,usecols=[score,target])
+    auc,ks,ks_score=get_roc(score_card,score,target,title)
+    plt.savefig('%s_roc.png'%tag)
+    get_cum_gains(score_card,score,target,title)
+    plt.savefig('%s_cum_gains.png'%tag)
+    get_precision_recall(score_card,score,target,title)
+    plt.savefig('%s_precision_recall.png'%tag)
+    decile_analysis=get_deciles_analysis(score_card,score,target)
+    decile_analysis.to_csv('%s_decile_analysis.csv'%tag)
+    pd.Series([auc,ks,ks_score],index=['auc','ks','ks_score']).to_csv('%s_summary.csv'%tag)
     
